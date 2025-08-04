6b0e8b921cbad93b4e37de7ab89579fa07b3d9d3
Cristian Negulescu <cristian@clamsen.com>
2025-08-04 20:47:55 +0100

    Fix Linux build

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ echo "Building OdinLogAnalyzer..."
 # Build for Linux
 cp ./src/resources/CascadiaCode/CaskaydiaCoveNerdFont-Regular.ttf ./Binary/MyFont.ttf
 cp ./src/resources/CascadiaCode/CaskaydiaCoveNerdFont-Regular.ttf ./MyFont.ttf
-odin build src -out:OdinLogAnalyzer
+odin build src -out:./Binary/OdinLogViewer
 
 if [ $? -eq 0 ]; then
     echo "Build successful!"
-    echo "Run with: ./OdinLogAnalyzer"
+    echo "Run with: ./OdinLogViewer"
     echo ""
-    echo "Note: To run without terminal, use: nohup ./OdinLogAnalyzer &"
+    echo "Note: To run without terminal, use: nohup ./OdinLogViewer &"
     echo "Or launch from your desktop environment's application launcher"
 else
     echo "Build failed!"
